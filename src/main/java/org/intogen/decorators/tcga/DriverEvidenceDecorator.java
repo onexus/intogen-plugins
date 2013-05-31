7433932e2dbb9bddefbefaf65dd32be2e214b761
Jordi Deu-Pons <jordi@jordeu.net>
2013-05-31 09:11:29 +0200

    Fix DRIVER_EVIDENCE decorator

--- src/main/java/org/intogen/decorators/tcga/DriverEvidenceDecorator.java
+++ src/main/java/org/intogen/decorators/tcga/DriverEvidenceDecorator.java
@@ -4,7 +4,6 @@ import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.EmptyPanel;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.util.string.Strings;
 import org.onexus.collection.api.Field;
 import org.onexus.collection.api.IEntity;
 import org.onexus.resource.api.ParameterKey;
@@ -22,35 +21,34 @@ public class DriverEvidenceDecorator extends FieldDecorator {
     @Override
     public void populateCell(WebMarkupContainer cellContainer, String componentId, IModel<IEntity> data) {
 
-        String evidence = Strings.toString(data.getObject().get("DRIVER_EVIDENCE"));
+        Integer count = (Integer) data.getObject().get("COUNT");
+        Integer oncodrive = (Integer) data.getObject().get("ONCODRIVE");
+        Integer oncoclust = (Integer) data.getObject().get("ONCOCLUST");
+        Integer activeDriver = (Integer) data.getObject().get("ACTIVE_DRIVER");
+        Integer music = (Integer) data.getObject().get("MUSIC");
+        Integer mutsig = (Integer) data.getObject().get("MUTSIG");
 
-        if (Strings.isEmpty(evidence)) {
+        if (count == null) {
             cellContainer.add(new EmptyPanel(componentId));
         } else {
 
             StringBuilder label = new StringBuilder();
 
-            for (char c : evidence.toUpperCase().toCharArray()) {
 
-                switch (c) {
-                    case 'C':
-                        label.append("<span class='badge badge-info' rel='tooltip' title='Gene with clustering bias detected by OncodriveCLUST'>C</span>&nbsp;");
-                        break;
-                    case 'F':
-                        label.append("<span class='badge badge-warning' rel='tooltip' title='Gene with functional impact bias detected by OncodriveFM'>F</span>&nbsp;");
-                        break;
-                    case 'A':
-                        label.append("<span class='badge badge-important' rel='tooltip' title='Gene with accumulation of mutations in active sites detected by ActiveDriver'>A</span>&nbsp;");
-                        break;
-                    case 'R':
-                        label.append("<span class='badge badge-success' rel='tooltip' title='Gene recurrently mutated detected by MuSiC'>R</span>&nbsp;");
-                        break;
-                    case 'M':
-                        label.append("<span class='badge badge-inverse' rel='tooltip' title='Gene recurrently mutated and with other evidences of selection detected by MutSig'>M</span>&nbsp;");
-                        break;
-                }
+            if (oncoclust != null && oncoclust.equals(1))
+                label.append("<span class='badge badge-info' rel='tooltip' title='Gene with clustering bias detected by OncodriveCLUST'>C</span>&nbsp;");
 
-            }
+            if (oncodrive != null && oncodrive.equals(1))
+                label.append("<span class='badge badge-warning' rel='tooltip' title='Gene with functional impact bias detected by OncodriveFM'>F</span>&nbsp;");
+
+            if (activeDriver != null && activeDriver.equals(1))
+                label.append("<span class='badge badge-important' rel='tooltip' title='Gene with accumulation of mutations in active sites detected by ActiveDriver'>A</span>&nbsp;");
+
+            if (music != null && music.equals(1))
+                label.append("<span class='badge badge-success' rel='tooltip' title='Gene recurrently mutated detected by MuSiC'>R</span>&nbsp;");
+
+            if (mutsig != null && mutsig.equals(1))
+                label.append("<span class='badge badge-inverse' rel='tooltip' title='Gene recurrently mutated and with other evidences of selection detected by MutSig'>M</span>&nbsp;");
 
             cellContainer.add(new Label(componentId, label.toString()).setEscapeModelStrings(false));
         }
