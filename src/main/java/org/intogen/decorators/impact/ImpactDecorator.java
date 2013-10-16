7344387548e0ea46970412ace03826cccba43e9f
Jordi Deu-Pons <jordi@jordeu.net>
2013-10-16 14:58:37 +0200

    Impact decorator contribute to query build

--- src/main/java/org/intogen/decorators/impact/ImpactDecorator.java
+++ src/main/java/org/intogen/decorators/impact/ImpactDecorator.java
@@ -4,10 +4,16 @@ import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.onexus.collection.api.Collection;
 import org.onexus.collection.api.Field;
 import org.onexus.collection.api.IEntity;
 import org.onexus.resource.api.Parameters;
 import org.onexus.website.api.widgets.tableviewer.decorators.utils.FieldDecorator;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class ImpactDecorator extends FieldDecorator {
 
@@ -25,4 +31,32 @@ public class ImpactDecorator extends FieldDecorator {
         cellContainer.add(new AttributeModifier("class", Model.of("st")));
     }
 
+    @Override
+    public List<String> getExtraFields(Collection collection) {
+
+        String fieldChr = parameters.get(ImpactDecoratorParameters.FIELD_CHR);
+        String fieldPosition = parameters.get(ImpactDecoratorParameters.FIELD_POSITION);
+        String fieldAllele = parameters.get(ImpactDecoratorParameters.FIELD_ALLELE);
+        String fieldGeneid = parameters.get(ImpactDecoratorParameters.FIELD_GENEID);
+
+        List<String> fields = new ArrayList<String>();
+
+        if (fieldChr!=null && collection.getField(fieldChr)!=null) {
+            fields.add(fieldChr);
+        }
+
+        if (fieldPosition!=null && collection.getField(fieldPosition)!=null) {
+            fields.add(fieldPosition);
+        }
+
+        if (fieldAllele!=null && collection.getField(fieldAllele)!=null) {
+            fields.add(fieldAllele);
+        }
+
+        if (fieldGeneid!=null && collection.getField(fieldGeneid)!=null) {
+            fields.add(fieldGeneid);
+        }
+
+        return fields;
+    }
 }
